9fbc93458a23d0fc9eb4f3e660d604042006d8eb
soheil <soheilasad2018@yahoo.com>
2019-05-17 16:12:21 +0430

    rename get_command to command_handeler and add singup and login syntax checking to it

---
+++ command_handeler.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <exception>
+#include <regex>
+
+#include "command_handeler.h"
+#include "exception.h"
+
+using namespace std;
+
+void CommandHandeler::split_up_in_space_place(string& all_words, int space_place)
+{
+	if (space_place == 0)
+		all_words.erase(0, 1);
+	else
+	{
+		string word = all_words.substr(0, space_place);
+		all_words.erase(0, space_place + 1);
+		command_line.push_back(word); // run time error
+	}
+}
+
+void CommandHandeler::separate_words_in_line(string all_words)
+{
+	for (int i = 0; i < all_words.size(); i++)
+	{
+		if (all_words[i] == ' ')
+		{
+			split_up_in_space_place(all_words, i);
+			i = -1;
+		}
+	}
+	command_line.push_back(all_words);
+}
+
+void CommandHandeler::read_line()
+{
+    string all_line;
+    getline(cin,all_line);
+    separate_words_in_line(all_line);
+    separate_command_elements();
+}
+
+void CommandHandeler::separate_command_elements()
+{
+    separate_command_head_elements();
+    for(int i = 2; i < command_line.size(); i+=2)
+        command_elements.insert(pair<string, string>(command_line[i], command_line[i+1]));
+}
+
+void CommandHandeler::separate_command_head_elements()
+{
+	command_elements.insert(pair<string, string>("order_type", command_line[0]));
+	if(command_line[2]=="?")
+	{
+		command_elements.insert(pair<string, string>("order", command_line[1]+command_line[2]));
+		command_line.erase(command_line.begin()+2,command_line.begin()+3);
+	}
+	else
+		command_elements.insert(pair<string, string>("order", command_line[1]));
+}
+
+
+
+std::map<std::string,std::string> CommandHandeler::get_command()
+{
+    return  command_elements;
+}
+
+void CommandHandeler::check_singup_syntax_correction()
+{
+	if(command_elements.size()%2 != 0)
+		throw BadRequest();
+
+	if(!command_elements.count("username"))
+		throw BadRequest();
+
+	const regex pattern
+    ("(\\w+)(\\.|_)?(\\w*)@(\\w+)");
+    if(!regex_match(command_elements["email"], pattern))
+        throw BadRequest();
+
+	try{
+        stoi(command_elements["password"]);
+		stoi(command_elements["age"]);
+    }catch(exception ex){
+        throw BadRequest();
+    }
+}
+void CommandHandeler::check_login_syntax_correction()
+{
+	if(command_elements.size() != 4)
+		throw BadRequest();
+
+	if(!command_elements.count("username"))
+		throw BadRequest();
+
+	try{
+        stoi(command_elements["password"]);
+    }catch(exception ex){
+        throw BadRequest();
+    }
+}
---
+++ command_handeler.h
@@ -0,0 +1,27 @@
+#ifndef CommandHandeler_H
+#define CommandHandeler_H
+
+#include <vector>
+#include <map>
+
+class CommandHandeler
+{
+public:
+    void read_line();
+    void separate_words_in_line(std::string all_line);
+    void split_up_in_space_place(std::string& all_words, int space_place);
+    void separate_command_elements();
+    void separate_command_head_elements();
+    std::map<std::string,std::string> get_command();
+    
+    
+    void check_singup_syntax_correction();
+    void check_login_syntax_correction();
+
+   
+private:
+    std::vector< std::string > command_line ;
+    std::map<std::string,std::string> command_elements ;
+};
+
+#endif
